ff716969b261beee37fcda2f6d3a94cc0d7686ca
1lDX <worrapon453@gmail.com>
2026-03-13 22:03:06 +0700

    feat(dashboard): add system card with cpu ram I/O

--- app/dashboard2/page.tsx
+++ app/dashboard2/page.tsx
@@ -18,6 +18,7 @@ import {
   DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { Card, CardContent } from "@/components/ui/card";
 
 const processes = [
   { pid: 1234, name: "node", cpu: 12, memory: 150, status: "running" },
@@ -53,10 +54,47 @@ export default function Page() {
         </DropdownMenu>
       </div>
 
-      <div className="p-7 round-lg bg-zinc-900 border-zinc-800">
-        <div className="">
+      <div className="grid grid-cols-3 gap-x-5 round-lg bg-zinc-900 border-zinc-800 px-7">
+        <section className="col-span-2">
           <ChartExample />
-        </div>
+        </section>
+        <section className="h-full">
+          <Card className="bg-[#252525] border-0 border-[#383838] h-full">
+            <CardContent className="text-white flex flex-col justify-between m-5 h-full">
+              <div>
+                <p className="text-md font-medium mb-1 opacity-80">CPU Usage</p>
+                <span className="flex items-baseline gap-2">
+                  <p className="text-3xl font-bold">67</p>
+                  <p className="text-md font-medium mb-1 opacity-80">
+                    %
+                  </p>
+                </span>
+              </div>
+              <div>
+                <p className="text-md font-medium mb-1 opacity-80">
+                  Memory Usage
+                </p>
+                <span className="flex items-baseline gap-2">
+                  <p className="text-3xl font-bold">12.4</p>
+                  <p className="text-md font-medium mb-1 opacity-80">
+                    GB / 64GB
+                  </p>
+                </span>
+              </div>
+              <div>
+                <p className="text-md font-medium mb-1 opacity-80">
+                  Disk I/O Usage
+                </p>
+                <span className="flex items-baseline gap-2">
+                  <p className="text-3xl font-bold">128</p>
+                  <p className="text-md font-medium mb-1 opacity-80">
+                    GB / 512GB
+                  </p>
+                </span>
+              </div>
+            </CardContent>
+          </Card>
+        </section>
       </div>
     </div>
   );
